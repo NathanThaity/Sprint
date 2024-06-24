3b8d5b790160de1a0355f1717586367d39fe6548
NathanThaity <159771197+NathanThaity@users.noreply.github.com>
2024-06-24 13:01:47 +0200

    SPRINT2

--- framework/src/main/java/com/framework/controller/FrontController.java
+++ framework/src/main/java/com/framework/controller/FrontController.java
@@ -1,24 +1,105 @@
 package com.framework.controllers;
-import java.io.IOException;
-import java.io.PrintWriter;
 
+import com.framework.annotations.GET;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-// javac -cp "C:\Program Files\Apache Software Foundation\Tomcat 10.1\webapps\Spring\WEB-INF\lib\*" FrontController.java
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FrontController extends HttpServlet {
-    
+
+    private Map<String, Mapping> urlMappings = new HashMap<>();
+
+    // Variable pour stocker le nom du package des contrôleurs
+    private String controllerPackage;
+
+    @Override
+    public void init() throws ServletException {
+        // Récupérer le nom du package des contrôleurs depuis les paramètres d'initialisation
+        controllerPackage = getServletConfig().getInitParameter("controller-package");
+        // Scanner les contrôleurs
+        scanControllers();
+    }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        // Extract the requested JSP page name from the URL
-        String requestedPage = request.getPathInfo();
-        
-        // Forward the request to the appropriate JSP page
-        // request.getRequestDispatcher("/web/page" + requestedPage).forward(request, response);
-        out.println("/WEB-INF/views/jsp" + requestedPage);
+
+        // Récupérer le chemin de l'URL de la requête
+        String requestURL = request.getPathInfo();
+
+        // Rechercher le Mapping associé au chemin URL de la requête
+        Mapping mapping = urlMappings.get(requestURL);
+
+        if (mapping != null) {
+            // Afficher le chemin URL et le Mapping
+            out.println("URL: " + requestURL);
+            out.println("Mapping: " + mapping);
+        } else {
+            // Afficher qu'il n'y a pas de méthode associée à ce chemin
+            out.println("No method associated with URL: " + requestURL);
+        }
+
         out.close();
+    }
+
+    private void scanControllers() {
+        try {
+            // Charger le class loader pour accéder aux classes du package
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            if (classLoader == null) {
+                classLoader = getClass().getClassLoader();
+            }
+
+            // Convertir le nom du package en chemin relatif pour le class loader
+            String packagePath = controllerPackage.replace(".", "/");
+
+            // Récupérer les ressources (fichiers .class) du package
+            Enumeration<URL> resources = classLoader.getResources(packagePath);
+            while (resources.hasMoreElements()) {
+                URL resource = resources.nextElement();
+                if (resource.getProtocol().equals("file")) {
+                    File packageDirectory = new File(resource.toURI());
+                    if (packageDirectory.isDirectory()) {
+                        // Parcourir les fichiers dans le répertoire du package
+                        File[] files = packageDirectory.listFiles();
+                        if (files != null) {
+                            for (File file : files) {
+                                // Vérifier si le fichier est une classe .class
+                                if (file.isFile() && file.getName().endsWith(".class")) {
+                                    // Charger la classe à partir du fichier
+                                    String className = file.getName().replace(".class", "");
+                                    Class<?> clazz = Class.forName(controllerPackage + "." + className);
+                                    // Parcourir les méthodes de la classe pour détecter les annotations GET
+                                    for (var method : clazz.getDeclaredMethods()) {
+                                        if (method.isAnnotationPresent(GET.class)) {
+                                            GET getAnnotation = method.getAnnotation(GET.class);
+                                            String url = getAnnotation.value();
+                                            urlMappings.put(url, new Mapping(clazz.getName(), method.getName()));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
